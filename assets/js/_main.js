b4b584375f18b2273e7c215c6089397f5ead0766
john4braham <john.abraham@hotmail.com>
2018-04-02 11:14:12 -0400

    Redesign for mobile view navigation

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,16 +11,22 @@ var changeClass = function (r,className1,className2) {
     }
     return r.className;
 };
+
 //  Creating our button in JS for smaller screens
 document.getElementById('site-nav').insertAdjacentHTML(
   'afterBegin',
-  '<button type="button" role="button" id="menutoggle" class="navtoggle navicon-lines-button x" aria-hidden="true"><span class="navicon-lines"></span>menu</button>'
+  '<button type="button" role="button" id="menutoggle" class="navtoggle navicon-lines-button x" aria-hidden="true">' +
+    '<span class="navicon-lines"></span>' +
+    '<span class="navicon-text">menu</span>' +
+  '</button>'
 );
 
 //  Toggle the class on click to show / hide the menu
 document.getElementById('menutoggle').onclick = function() {
   changeClass(this, 'navtoggle active', 'navtoggle');
+  $('.top-navigation-mobile').slideToggle();
 };
+
 // http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/comment-page-2/#comment-438918
 document.onclick = function(e) {
   var mobileButton = document.getElementById('menutoggle'),
